c12d3ff36a690a16a118dd79326f33885a59e9d8
sheshank <akula.sheshank999@gmail.com>
2019-06-08 17:44:31 +0530

    changes are made please check

--- src/main/java/com/prac/InstaController.java
+++ src/main/java/com/prac/InstaController.java
@@ -3,6 +3,6 @@ package com.prac;
 public class InstaController {
 
 	public static void main(String[] args) {
-		System.out.println("hi am instagram!! done!!");
+		System.out.println("hi am instagram!! ");
 	}
 }
