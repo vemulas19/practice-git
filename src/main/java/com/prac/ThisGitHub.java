a25bd6b32cf5ad643369e1532d396f1516d16ef6
srisathya <srisathya@192.168.0.15>
2019-06-08 11:56:16 -0700

    number

--- src/main/java/com/prac/ThisGitHub.java
+++ src/main/java/com/prac/ThisGitHub.java
@@ -3,6 +3,7 @@ package com.prac;
 public class ThisGitHub {
 public String Calculator(){
 	System.out.println("This is jAVA calculator");
+	System.out.println("enter a number");
 	return "";
 	
 }public static void main(String[] args) {
