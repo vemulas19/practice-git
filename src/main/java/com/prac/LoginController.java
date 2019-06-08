09bdcb459003d965a38444ba5604a089c66b31e5
HP <HP@vineshGuggilla>
2019-06-08 10:14:59 +0530

    hi

--- src/main/java/com/prac/LoginController.java
+++ src/main/java/com/prac/LoginController.java
@@ -16,7 +16,7 @@ public class LoginController {
 		System.out.println(" then do profile page implementation  ");
 
 		System.out.println("hello");
-		System.out.println("follow coding standrads" );
+		System.out.println("follow coding standrads");
 		System.out.println("hi am github !!" );
 
 
