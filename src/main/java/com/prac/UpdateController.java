15d99f9008a1d75fd4dcd3969f470bbb1c82b613
srisathya <srisathya@192.168.0.15>
2019-06-08 11:16:04 -0700

    Hello

--- src/main/java/com/prac/UpdateController.java
+++ src/main/java/com/prac/UpdateController.java
@@ -15,7 +15,7 @@ import com.pojos.SpringPojo;
 
 public class Mavenmobile {
 	public Boolean isValidMobile(String number){
-		  Pattern p = Pattern.compile("(0/91)?[7-9][0-9]{9}"); 
+		  Pattern p = Pattern.compile("(0/91)?[7-9][0-9]{9}" ); 
 		   
 	      Matcher m = p.matcher(number); 
 	      return (m.find() && m.group().equals(number)); 
