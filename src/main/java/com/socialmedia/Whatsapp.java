fb924d701794ae1ea058bf6d57683351d3370bca
Admin <Admin@DESKTOP-MKHNVM5>
2019-06-08 18:36:55 +0530

    implementation done

--- src/main/java/com/socialmedia/Whatsapp.java
+++ src/main/java/com/socialmedia/Whatsapp.java
@@ -2,6 +2,6 @@ package com.socialmedia;
 
 public class Whatsapp {
 public static void main(String[] args) {
-	System.out.println("welcome to task1");
+	System.out.println("welcome to Whatsapp@!");
 }
 }
