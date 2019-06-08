b23b0b785819fe5e2fe8b89712e968a4956dc02e
gouth <gouth@GouthaMKumar>
2019-06-08 09:47:54 +0530

    Tested

--- src/main/java/com/prac/TestGit.java
+++ src/main/java/com/prac/TestGit.java
@@ -3,7 +3,7 @@ package com.prac;
 public class TestGit {
 	
 	public static void main(String[] args) {
-		System.out.println("sucessfully Tested Git ");
+		System.out.println("sucessfully Tested Git");
 	}
 
 }
