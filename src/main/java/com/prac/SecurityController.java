cb9dd0d5487e265461e4f8f34f126bdf9d24f536
HP <HP@vineshGuggilla>
2019-06-08 09:12:18 +0530

    done with security

--- src/main/java/com/prac/SecurityController.java
+++ src/main/java/com/prac/SecurityController.java
@@ -3,6 +3,6 @@ package com.prac;
 public class SecurityController {
 
 	public static void main(String[] args) {
-		System.out.println("security check implemented!!");
+		System.out.println("security check implemented!!  ");
 	}
 }
