259e038f762c319e8b9c63aa75ad83d755b8afe5
user <user@DESKTOP-PRFGI0V>
2019-06-08 10:22:16 +0530

    hch

--- src/main/java/com/prac/EditTest.java
+++ src/main/java/com/prac/EditTest.java
@@ -2,7 +2,7 @@ package com.prac;
 
 public class EditTest {
 	public static void main(String[] args) {
-		System.out.println("updated completly!!");
+		System.out.println("updated completly@!!");
 	}
 
 }
