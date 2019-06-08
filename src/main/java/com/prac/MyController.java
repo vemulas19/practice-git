d96cddf915856c9446e5aeb71cde55ae6dd34931
dell <dell@Prashanth>
2019-06-08 18:32:56 +0530

    Auto generated stub has removed.

--- src/main/java/com/prac/MyController.java
+++ src/main/java/com/prac/MyController.java
@@ -3,7 +3,7 @@ package com.prac;
 public class MyController {
 
 	public MyController() {
-		// TODO Auto-generated constructor stub
+		System.out.println("my controller Constructor");
 	}
 public static void main(String[] args) {
 	System.out.println("this is my Controller,kindly don't edit this controller");
