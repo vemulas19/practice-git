92411f8db930715396b773c5204b0125ac909a86
HP <HP@vineshGuggilla>
2019-06-08 17:35:02 +0530

    implemented whatsapp in different package

---
+++ src/main/java/com/socialmedia/User.java
@@ -0,0 +1,33 @@
+package com.socialmedia;
+
+public class User {
+
+	private int userId;
+	private String name;
+	private String city;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
---
+++ src/main/java/com/socialmedia/Whatsapp.java
@@ -0,0 +1,9 @@
+package com.socialmedia;
+
+public class Whatsapp {
+
+	public String SaveUser(User user) {
+		System.out.println("City is : " + user.getCity());
+		return "success";
+	}
+}
