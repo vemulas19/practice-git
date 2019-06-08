bfd8f54cbd052cdddca265224e08ee478190642f
user <user@DESKTOP-PRFGI0V>
2019-06-08 18:43:48 +0530

    Employee Details generated

---
+++ src/main/java/com/employee/EmployeePojo.java
@@ -0,0 +1,34 @@
+package com.employee;
+
+public class EmployeePojo {
+
+	private Integer id;
+	private String empName;
+	private String empBranch;
+	private String empLocation;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpBranch() {
+		return empBranch;
+	}
+	public void setEmpBranch(String empBranch) {
+		this.empBranch = empBranch;
+	}
+	public String getEmpLocation() {
+		return empLocation;
+	}
+	public void setEmpLocation(String empLocation) {
+		this.empLocation = empLocation;
+	}
+	
+}
---
+++ src/main/java/com/employee/TestEmployee.java
@@ -0,0 +1,9 @@
+package com.employee;
+
+public class TestEmployee {
+	public void saveEmployee(EmployeePojo e){
+		System.out.println("Employee detail:"+e.getEmpBranch());
+
+	}
+}
+
