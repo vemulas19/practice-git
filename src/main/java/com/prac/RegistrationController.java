6341b2e7f895b1068dd6c57ca0e7089c1131ceeb
Admin <Admin@DESKTOP-MKHNVM5>
2019-06-08 10:16:29 +0530

    implemented

--- src/main/java/com/prac/RegistrationController.java
+++ src/main/java/com/prac/RegistrationController.java
@@ -4,7 +4,7 @@ public class RegistrationController {
 
 	public String saveUser() {
 		System.out.println("Need to implement saveUser here!!");
-	System.out.println("added validation jar successfully ");
+	System.out.println("added validation jar successfully!! ");
 	System.out.println("save user has implemented");
 		return "login";
 	}
