85366c67f3f19c95c894a69d6ef7de6fa3b5fe5c
sheshank <akula.sheshank999@gmail.com>
2019-06-08 09:02:42 +0530

    added validaiton for registration flow

--- src/main/java/com/prac/RegistrationController.java
+++ src/main/java/com/prac/RegistrationController.java
@@ -4,6 +4,7 @@ public class RegistrationController {
 
 	public String saveUser() {
 		System.out.println("Need to implement saveUser here!!");
+		System.out.println("added validation logic!!");
 		return "login";
 	}
 }
