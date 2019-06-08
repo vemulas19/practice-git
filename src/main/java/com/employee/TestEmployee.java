07dd265fdd10be3451d60284a1d4186f7dd8771c
user <user@DESKTOP-PRFGI0V>
2019-06-08 19:04:26 +0530

    modified successfully

--- src/main/java/com/employee/TestEmployee.java
+++ src/main/java/com/employee/TestEmployee.java
@@ -3,7 +3,11 @@ package com.employee;
 public class TestEmployee {
 	public void saveEmployee(EmployeePojo e){
 		System.out.println("Employee detail:"+e.getEmpBranch());
+		
 
+	}
+	public static void main(String[] args) {
+		System.out.println("Employee details successfully inserted");
 	}
 }
 
