ec4ef7d55153bd5565b83e2cf65c7e7b07a472d6
Admin <Admin@DESKTOP-MKHNVM5>
2019-06-08 10:10:50 +0530

    implemented in  login

--- src/main/java/com/prac/LoginController.java
+++ src/main/java/com/prac/LoginController.java
@@ -5,7 +5,7 @@ public class LoginController {
 	public static void main(String[] args) {
 		System.out.println("hi this is login implementation!!");
 
-		System.out.println("welcome to GitHub world !!");
+		System.out.println("welcome to GitHub world @ !!");
 
 		System.out.println("do practice more to get more knowledge on git ");
 
