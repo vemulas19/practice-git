abdf6d06ebe6d5910eaf7f9a694019cbc228265d
HP <HP@vineshGuggilla>
2019-06-08 17:49:10 +0530

    done with review comments.

--- src/main/java/com/socialmedia/User.java
+++ src/main/java/com/socialmedia/User.java
@@ -3,7 +3,7 @@ package com.socialmedia;
 public class User {
 
 	private int userId;
-	private String name;
+	private String userName;
 	private String city;
 
 	public int getUserId() {
@@ -14,12 +14,12 @@ public class User {
 		this.userId = userId;
 	}
 
-	public String getName() {
-		return name;
+	public String getUserName() {
+		return userName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 
 	public String getCity() {
--- src/main/java/com/socialmedia/Whatsapp.java
+++ src/main/java/com/socialmedia/Whatsapp.java
@@ -2,8 +2,12 @@ package com.socialmedia;
 
 public class Whatsapp {
 
-	public String SaveUser(User user) {
+	public String saveUser(User user) {
 		System.out.println("City is : " + user.getCity());
 		return "success";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("test main");
 	}
 }
