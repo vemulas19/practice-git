2a51b4950b603bf019106e1c2327286640c06f34
dell <dell@dell-PC>
2019-06-08 09:45:50 +0530

    do practice

--- src/main/java/com/prac/LoginController.java
+++ src/main/java/com/prac/LoginController.java
@@ -5,7 +5,7 @@ public class LoginController {
 	public static void main(String[] args) {
 		System.out.println("hi this is login implementation!!");
 
-		System.out.println("do practice more to get more knowledge on git");
+		System.out.println("do practice more to get more knowledge on git ");
 
 
 		System.out.println(" then do profile page implementation ");
