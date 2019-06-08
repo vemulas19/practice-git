daf26956c404ac7564e1568e940d5c8f7220541b
ADMIN <ADMIN@DESKTOP-9K98DUA>
2019-06-08 18:46:36 +0530

    task twitter registration finished...

---
+++ src/main/java/com/twitter/Registercontroller.java
@@ -0,0 +1,16 @@
+package com.twitter;
+
+public class Registercontroller {
+	
+	public static void main(String[] args) {
+		save();
+		
+	}
+
+	private static String save() {
+System.out.println("welcome to Twitter Registration Page");
+return "Login";
+
+	}
+
+}
---
+++ src/main/java/com/twitter/TwitterPojo.java
@@ -0,0 +1,28 @@
+package com.twitter;
+
+public class TwitterPojo {
+
+	
+	private String UserName;
+	private String Password;
+	private String Location;
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getLocation() {
+		return Location;
+	}
+	public void setLocation(String location) {
+		Location = location;
+	}
+	
+}
