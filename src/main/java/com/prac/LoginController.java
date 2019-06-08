4f2fbba816ad2f6bcd1905e3060a03357273cead
dell <dell@dell-PC>
2019-06-08 09:41:55 +0530

    do Practice more in git

--- src/main/java/com/prac/LoginController.java
+++ src/main/java/com/prac/LoginController.java
@@ -4,5 +4,6 @@ public class LoginController {
 
 	public static void main(String[] args) {
 		System.out.println("hi this is login implementation!!");
+		System.out.println("do practice more to get more knowledge on git");
 	}
 }
