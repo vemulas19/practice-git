395c9226249760559ee67ef2a8c1d3f85bbc5b0a
Anvesh Gajula <Anvesh Gajula@Gajulas>
2019-06-08 18:20:55 +0530

    Implemented Mail class

---
+++ src/main/java/com/gmail/MailController.java
@@ -0,0 +1,11 @@
+package com.gmail;
+
+public class MailController {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Testing for the mail Implementation......");
+	}
+
+}
