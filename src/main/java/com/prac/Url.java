e24ab3fe55f076a54b7dcdd29f34277c80104643
dell <dell@Prashanth>
2019-06-08 19:01:34 +0530

    changed static

--- src/main/java/com/prac/Url.java
+++ src/main/java/com/prac/Url.java
@@ -4,14 +4,15 @@ public class Url {
 
 	private int a;
 	private int b;
-	static int c;
+	  
 	public Url(Integer a,Integer b){
 		this.a=a;
 		this.b=b;
-		c=a+b;
+		int c=a+b;
+		System.out.println(c);
 	}
 	public static void main(String[] args) {
 		Url u=new Url(10,12);
-		System.out.println(c);
+		
 	}
 }
