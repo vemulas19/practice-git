8f026022caddb6a8faa6741a11e8151a02bf23c1
dell <dell@Prashanth>
2019-06-08 18:24:43 +0530

    My Controller added

---
+++ src/main/java/com/prac/MyController.java
@@ -0,0 +1,11 @@
+package com.prac;
+
+public class MyController {
+
+	public MyController() {
+		// TODO Auto-generated constructor stub
+	}
+public static void main(String[] args) {
+	System.out.println("this is my Controller,kindly don't edit this controller");
+}
+}
