51c0e8b8ad4729f8edbea294d1f7487099b330ec
ADMIN <ADMIN@DESKTOP-9K98DUA>
2019-06-08 18:32:47 +0530

    edited twice task1

--- src/main/java/com/prac/Task1.java
+++ src/main/java/com/prac/Task1.java
@@ -3,6 +3,6 @@ package com.prac;
 public class Task1 {
 
 	public void sum() {
-		System.out.println("task1 implementation done!! THANX");
+		System.out.println("task1 implementation done!! THANX +2");
 	}
 }
