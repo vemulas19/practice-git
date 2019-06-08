53e7cefcd1f44521f24fac8c4bbf5ce154ea3f8f
srisathya <srisathya@srisathya-PC>
2019-06-08 10:09:38 -0700

    implemented in LoginController

--- src/main/java/com/prac/LoginController.java
+++ src/main/java/com/prac/LoginController.java
@@ -8,7 +8,7 @@ public class LoginController {
 		System.out.println("welcome to GitHub world !!");
 
 		System.out.println("do practice more to get more knowledge on git ");
-		System.out.println("HELLO");
+		System.out.println("HELLO User");
 
 
 		System.out.println(" then mbmnbmbmdo profile page implementation  ");
