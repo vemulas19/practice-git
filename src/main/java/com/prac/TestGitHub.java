d244faeba3b3b6456d9826834b12f53fea3dccf2
srisathya <srisathya@192.168.0.15>
2019-06-08 11:51:16 -0700

    done

--- src/main/java/com/prac/TestGitHub.java
+++ src/main/java/com/prac/TestGitHub.java
@@ -2,7 +2,7 @@ package com.prac;
 
 public class TestGitHub {
 public static void main(String[] args) {
-	System.out.println("hi GitHub!! ");
+	System.out.println("hi GitHub!! s "); 
 	System.out.println("hello from git");
 	System.out.println("hello");
 }
