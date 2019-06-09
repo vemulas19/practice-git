3b3d22cc5f9d901d44d8d475804ab34fda470a41
gouth <gouth@GouthaMKumar>
2019-06-09 12:16:13 +0530

    Asus implementation done....!!!!

--- src/main/java/com/asus/mobiles/ZenfoneM2Pro.java
+++ src/main/java/com/asus/mobiles/ZenfoneM2Pro.java
@@ -2,4 +2,37 @@ package com.asus.mobiles;
 
 public class ZenfoneM2Pro {
 
-}
+		private String MobileSerialNumber;
+		private String MobileImeiNumber;
+		
+		
+		public String getMobileSerialNumber() {
+			return MobileSerialNumber;
+		}
+
+		public void setMobileSerialNumber(String mobileSerialNumber) {
+			MobileSerialNumber = mobileSerialNumber;
+		}
+
+		public String getMobileImeiNumber() {
+			return MobileImeiNumber;
+		}
+
+		public void setMobileImeiNumber(String mobileImeiNumber) {
+			MobileImeiNumber = mobileImeiNumber;
+		}
+
+		
+		
+		public static void main(String[] args) {
+			
+			ZenfoneM2Pro zn=new ZenfoneM2Pro();
+			zn.setMobileSerialNumber("4568-7528-4528-1592");
+			zn.setMobileImeiNumber("1200-4520-7895-7538-7528");
+			
+			System.out.println(zn.getMobileSerialNumber()+"-----"+zn.getMobileImeiNumber());
+			
+		}
+	}
+	
+	
