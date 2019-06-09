e67cfa5c2c5d0e503fb392bfc545b9d784b0f38a
Ravali <Ravali@DESKTOP-CE3CCQ5>
2019-06-09 13:08:57 +0530

    insta done

--- src/main/java/com/prac/InstaController.java
+++ src/main/java/com/prac/InstaController.java
@@ -4,10 +4,10 @@ public class InstaController {
 
 	public static void main(String[] args) {
 		System.out.println("hi am instagram!! ");
-<<<<<<< HEAD
+
 		System.out.println("instagram successful!");
-=======
+
 		System.out.println("hello!");
->>>>>>> refs/heads/master
+
 	}
 }
