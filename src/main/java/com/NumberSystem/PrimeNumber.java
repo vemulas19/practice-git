c095e3258bb39bbf46fe46ceab374388572b14d3
sathish <sathish@DESKTOP-VDIPNKO>
2019-06-09 11:04:30 -0700

    prime Number implemented

---
+++ src/main/java/com/NumberSystem/PrimeNumber.java
@@ -0,0 +1,28 @@
+package com.NumberSystem;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	
+	public static void main(String[] args) {
+		
+		int num,l;
+		boolean flag=false;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter ant integer value");
+		num = sc.nextInt();
+		
+		for(l=2;l<=(num/2);l++) {
+			if(num%l==0){
+				flag=true;
+				break;
+			}
+		}
+		if(flag==false)
+			System.out.println(num +"is a prime number");
+		else
+			System.out.println(num +"is not aprime number ");
+	}
+
+}
