c172f161bbef778069deeb9392145cefa3a805d2
user <user@DESKTOP-PRFGI0V>
2019-06-09 11:05:08 +0530

    success

--- src/main/java/com/prac/InstaController.java
+++ src/main/java/com/prac/InstaController.java
@@ -4,6 +4,6 @@ public class InstaController {
 
 	public static void main(String[] args) {
 		System.out.println("hi am instagram!! ");
-		System.out.println("hello");
+		System.out.println("hello!");
 	}
 }
