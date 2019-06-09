f11142ca95570d69a50b6308abc7bcc3ba270212
Anvesh Gajula <Anvesh Gajula@Gajulas>
2019-06-09 12:05:01 +0530

    Implemented asus

--- src/main/java/com/asus/mobiles/ZenfoneM2Pro.java
+++ src/main/java/com/asus/mobiles/ZenfoneM2Pro.java
@@ -1,5 +1,36 @@
 package com.asus.mobiles;
 
 public class ZenfoneM2Pro {
+	
+	private String mobileName;
+	private Integer mobileModel;
+	
+	
+	
+	public String getMobileName() {
+		return mobileName;
+	}
+	public void setMobileName(String mobileName) {
+		this.mobileName = mobileName;
+	}
+	public Integer getMobileModel() {
+		return mobileModel;
+	}
+	public void setMobileModel(Integer mobileModel) {
+		this.mobileModel = mobileModel;
+	}
+	
+	
 
+	
+	public static void main(String[] args) {
+		
+		ZenfoneM2Pro zn=new ZenfoneM2Pro();
+		zn.setMobileName("Asus");
+		zn.setMobileModel(2345);
+		
+		System.out.println(zn.getMobileName()+"--"+zn.getMobileModel());
+		
+		
+	}
 }
