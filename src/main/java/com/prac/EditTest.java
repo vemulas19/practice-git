76a9e0ae544d8aa9a809eced6a9efb944e9819c1
user <user@DESKTOP-PRFGI0V>
2019-06-09 12:04:39 +0530

    success

--- src/main/java/com/prac/EditTest.java
+++ src/main/java/com/prac/EditTest.java
@@ -3,7 +3,7 @@ package com.prac;
 public class EditTest {
 	public static void main(String[] args) {
 		System.out.println("updated completly@fdgsdg!!");
-		System.out.println("HELLO");
+		System.out.println("hello");
 	}
 
 }
