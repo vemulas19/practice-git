125f2319ab8b472e493c0e63527c1501e9e144c8
Lenovo <Lenovo@LAPTOP-P3E9R59F>
2019-06-09 11:57:08 +0530

    m1 is implemented

---
+++ src/main/java/com/employee/Implemetations.java
@@ -0,0 +1,16 @@
+package com.employee;
+
+public class Implemetations {
+
+	public static void m1() {
+		
+		System.out.println("m1 is implemented");
+		
+		}
+	
+	public static void main(String[] args) {
+		m1();
+	}
+	
+	
+}
