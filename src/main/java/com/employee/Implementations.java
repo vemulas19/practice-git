d695394efa33e1cc430c4f43f47b50b978a3698d
Lenovo <Lenovo@LAPTOP-P3E9R59F>
2019-06-09 12:16:15 +0530

    m1 method is implemented.

---
+++ src/main/java/com/employee/Implementations.java
@@ -0,0 +1,16 @@
+package com.employee;
+
+public class Implementations {
+	
+	public static void main(String[] args) {
+		m1();
+	}
+
+	private static void m1() {
+		System.out.println("m1 method is implmented");
+		
+	}
+	
+	
+
+}
