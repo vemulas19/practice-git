ca67e6a07a05696cd3f1cb548d9bb81450de65d5
Goutham2211 <51500844+Goutham2211@users.noreply.github.com>
2019-06-09 13:08:35 +0530

    moto

--- src/main/java/com/motorola/MotorolaImpl.java
+++ src/main/java/com/motorola/MotorolaImpl.java
@@ -1,5 +1,39 @@
 package com.motorola;
 
 public class MotorolaImpl {
+	
+	private String Model;
+	
+	private String SerialNo;
+
+	public String getModel() {
+		return Model;
+	}
+
+	public void setModel(String model) {
+		Model = model;
+	}
+
+	public String getSerialNo() {
+		return SerialNo;
+	}
+
+	public void setSerialNo(String serialNo) {
+		SerialNo = serialNo;
+	}
+	
+	public static void main(String[] args) {
+		
+		MotorolaImpl mo=new MotorolaImpl();
+		
+		mo.setModel("G4");
+		mo.setSerialNo("1234567895235625");
+		
+		System.out.println(mo.getModel()+"----"+mo.SerialNo);
+		
+		
+		
+		
+	}
 
 }
