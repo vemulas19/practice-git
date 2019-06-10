cd9453d308656025571afc9c59dd8741ce95c0aa
HP <HP@vineshGuggilla>
2019-06-10 20:06:37 +0530

    added properties

--- src/main/java/com/acer/AcerImpl.java
+++ src/main/java/com/acer/AcerImpl.java
@@ -2,4 +2,23 @@ package com.acer;
 
 public class AcerImpl {
 
+	private int empId;
+	private String name;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
