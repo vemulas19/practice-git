e328aee22f4e0df099bd0be79987468c1256192d
Anvesh Gajula <Anvesh Gajula@Gajulas>
2019-06-10 18:11:24 +0530

    Acer Specs

--- src/main/java/com/acer/AcerJava.java
+++ src/main/java/com/acer/AcerJava.java
@@ -1,5 +1,36 @@
 package com.acer;
 
 public class AcerJava {
+	
+	
+	private String modelName;
+	private String ramCapacity;
+	
+	
+	public String getModelName() {
+		return modelName;
+	}
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+	public String getRamCapacity() {
+		return ramCapacity;
+	}
+	public void setRamCapacity(String ramCapacity) {
+		this.ramCapacity = ramCapacity;
+	}
+	
+	public static void main(String[] args) {
+		
+		AcerJava aj=new AcerJava();
+		
+		aj.setModelName("AspireE1531");
+		aj.setRamCapacity("6 GB RAM");
+		
+		System.out.println(aj.getModelName()+"--"+aj.getRamCapacity());
+		
+	}
+	
+	
 
 }
