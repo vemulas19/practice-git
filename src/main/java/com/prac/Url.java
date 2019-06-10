f55b99e9b35489f10427717ad9baed56aae648ff
dell <dell@192.168.0.2>
2019-06-10 15:17:26 +0530

    One more Sysout Added

--- src/main/java/com/prac/Url.java
+++ src/main/java/com/prac/Url.java
@@ -4,12 +4,14 @@ public class Url {
 
 	private int a;
 	private int b;
+	private int c;
 	  
 	public Url(Integer a,Integer b){
 		this.a=a;
 		this.b=b;
 		int c=a+b;
 		System.out.println(c);
+		System.out.println(this.c);
 	}
 	public static void main(String[] args) {
 		Url u=new Url(10,12);
