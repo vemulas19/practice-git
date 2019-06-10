98861d2bd5599ad3c504768cbcc9933bcfade65d
Admin <Admin@192.168.0.10>
2019-06-10 23:37:36 +0530

    commited

---
+++ src/main/java/com/acer/AcerImpl.java
@@ -0,0 +1,21 @@
+package com.acer;
+
+public class AcerImpl {
+
+	private Integer eid;
+	private String ename;
+	public Integer getEid() {
+		return eid;
+	}
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+	
+}
