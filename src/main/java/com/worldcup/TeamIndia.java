a45a277eaae8991844e95aaa162e6e22b8466e50
Anvesh Gajula <Anvesh Gajula@Gajulas>
2019-06-11 17:47:03 +0530

    Team india is ready

--- src/main/java/com/worldcup/TeamIndia.java
+++ src/main/java/com/worldcup/TeamIndia.java
@@ -5,7 +5,25 @@ public class TeamIndia {
 	private String Captain;
 	private String viceCaptain;
 	private String wicketKeeper;
+	private String Keeper;
+	private Integer bowler_Count;
 	
+	
+	
+	
+	
+	public String getKeeper() {
+		return Keeper;
+	}
+	public void setKeeper(String keeper) {
+		Keeper = keeper;
+	}
+	public Integer getBowler_Count() {
+		return bowler_Count;
+	}
+	public void setBowler_Count(Integer bowler_Count) {
+		this.bowler_Count = bowler_Count;
+	}
 	public String getCaptain() {
 		return Captain;
 	}
@@ -25,5 +43,21 @@ public class TeamIndia {
 		this.wicketKeeper = wicketKeeper;
 	}
 	
+	
+	public static void main(String[] args) {
+		
+		
+		TeamIndia india = new TeamIndia();
+		
+		india.setCaptain("Rohith");
+		india.setCaptain("Kohli");
+		
+		
+		
+		System.out.println(india.getCaptain()+"--"+india.getViceCaptain());
+		
+		
+		
+	}
 
 }
