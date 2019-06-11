2c71c803edf33ee907b8b0d116088023951f1a66
prashanthY <prashanth11@DESKTOP-VDIPNKO>
2019-06-10 17:21:10 -0700

    # WARNING: head commit changed in the meantime
    all done

--- src/main/java/com/oneplus/OnePlusImpl.java
+++ src/main/java/com/oneplus/OnePlusImpl.java
@@ -1,5 +1,33 @@
 package com.oneplus;
 
+import org.apache.log4j.chainsaw.Main;
+
 public class OnePlusImpl {
+	
+	private String mobileIMEI;
+	private String mobileCompany;
+	
+	public String getMobileIMEI() {
+		return mobileIMEI;
+	}
+	public void setMobileIMEI(String mobileIMEI) {
+		this.mobileIMEI = mobileIMEI;
+	}
+	public String getMobileCompany() {
+		return mobileCompany;
+	}
+	public void setMobileCompany(String mobileCompany) {
+		this.mobileCompany = mobileCompany;
+	}
+	
+	public static void main(String[] args) {
+		
+		OnePlusImpl oi = new OnePlusImpl();
+		oi.setMobileCompany("Samsung");
+		oi.setMobileIMEI("123-456-789");
+		
+		System.out.println(oi.getMobileCompany()+" "+oi.getMobileIMEI());
+		
+	}
 
 }
